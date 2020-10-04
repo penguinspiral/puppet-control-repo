a368c61394a4a1807a24273e70671219f3224738
Myles Grindon <penguinspiral@users.noreply.github.com>
2020-10-03 19:37:49 -0700

    [puppet] Create Puppet 'profiles::bootstrap::node' subclass
    The 'profiles::bootstrap::node' subclass operates as a sub-meta-profile
    for further subclasses: 'profiles::bootstrap::node::agent' and
    'profiles::bootstrap::node::master'.
    
    To facilitate the eventual adoption of dedicated, load-balanced Puppet
    master instances this module has been designed with the Puppet agent and
    Puppet master as discrete configurable components.
    
    By default each node operates in a standalone, "serverless" manner
    running both a Puppet agent (mandatory) and a "local" (i.e. listening on
    `localhost`) Puppet master (optional) daemon.

---
+++ site/profiles/manifests/bootstrap/node.pp
@@ -0,0 +1,18 @@
+# @summary 
+#   Specifies how a targeted node's Puppet agent obtains its catalog
+#   Supports both a client-only and standalone (a.k.a. serverless) configuration
+#
+# @example
+#   include profiles::bootstrap
+#
+# @param serverless
+#   Installs and configures a locally ran Puppet master for catalog generation
+#
+class profiles::bootstrap::node(
+  Boolean $serverless = true,
+){
+  include profiles::bootstrap::node::agent
+  if ($serverless) {
+    include profiles::bootstrap::node::master
+  }
+}
