3f7c441b72ce2fdde203a593cf2d079f303c76cc
Myles Grindon <penguinspiral@users.noreply.github.com>
2020-12-23 16:19:11 -0800

    [puppet] Include client/server ssh configuration file management
    The wrapped component module, 'ghoneycutt/ssh', is hard coded to manage
    both the SSH server ('sshd_config') and SSH client ('ssh_config')
    configuration files.
    
    This commit leverages 'ghoneycutt/ssh' parameters 'ssh_config_path' and
    'sshd_config_path' to specify unused (albeit managed) placeholder
    configuration files on a per-client, per-server basis.

--- site/profiles/manifests/ssh.pp
+++ site/profiles/manifests/ssh.pp
@@ -10,6 +10,20 @@
 #   Specify the OpenSSH server (sshd) service state
 #   Wrapper parameter: 'ghoneycutt-ssh' module class parameter
 #
+# @param manage_sshd_config
+#   Specify whether this module manages the OpenSSH server configuration
+#
+# @param manage_ssh_config
+#   Specify whether this module manages the OpenSSH client configuration
+#
+# @param sshd_config_path
+#   Specify the absolute path to the OpenSSH server configuration file
+#   Wrapper parameter: 'ghoneycutt-ssh' module class parameter
+#
+# @param ssh_config_path
+#   Specify the absolute path to the OpenSSH client configuration file
+#   Wrapper parameter: 'ghoneycutt-ssh' module class parameter
+#
 # @param permit_root_login
 #   Specify the manner in which the 'root' user can access the host
 #   Ref: man sshd_config(5) ~ 'PermitRootLogin'
@@ -29,6 +43,10 @@
 #
 class profiles::ssh(
   Stdlib::Ensure::Service                                       $service_ensure               = 'running',
+  Boolean                                                       $manage_sshd_config           = true,
+  Boolean                                                       $manage_ssh_config            = false,
+  Stdlib::Absolutepath                                          $sshd_config_path             = '/etc/ssh/sshd_config',
+  Stdlib::Absolutepath                                          $ssh_config_path              = '/etc/ssh/ssh_config',
   Enum['yes', 'no', 'without-password', 'forced-commands-only'] $permit_root_login            = 'no',
   Enum['yes', 'no']                                             $sshd_password_authentication = 'no',
   Enum['yes', 'no']                                             $sshd_pubkeyauthentication    = 'yes',
@@ -38,8 +56,21 @@ class profiles::ssh(
     fail('No SSH public key referenced and password authentication is disabled!')
   }
 
+  # Facilitate client/server only management by file substitution
+  $sshd_config_path_real = $manage_sshd_config ? {
+    true  => $sshd_config_path,
+    false => "${sshd_config_path}.fake",
+  }
+
+  $ssh_config_path_real = $manage_ssh_config ? {
+    true  => $ssh_config_path,
+    false => "${ssh_config_path}.fake",
+  }
+
   class { 'ssh':
     service_ensure               => $service_ensure,
+    sshd_config_path             => $sshd_config_path_real,
+    ssh_config_path              => $ssh_config_path_real,
     hiera_merge                  => false,
     permit_root_login            => $permit_root_login,
     sshd_password_authentication => $sshd_password_authentication,
