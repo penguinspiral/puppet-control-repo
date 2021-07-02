31dddd82bd839821dce38f2a4790da8545385703
Myles Grindon <penguinspiral@users.noreply.github.com>
2021-07-02 02:10:08 -0700

    [puppet] Parameterise bootstrap::node::server config & r10k
    Migrating from the Debian maintained Puppet master v5.5.10 to the
    official upstream Puppetserver v7.2.0 resulted in a change of
    configuration paths and configuration file formats (amongst other
    things).
    
    The global Puppet configuration file, 'puppet.conf', URI is now sourced
    from the globally accessible `$settings` namespace by default. To avoid
    modifying the manifest in the future the 'puppet.conf' URI can be
    explicitly specified as a class parameter value.

--- site/profiles/manifests/bootstrap/node/server.pp
+++ site/profiles/manifests/bootstrap/node/server.pp
@@ -1,11 +1,30 @@
 # @summary
-#   Configures a Puppet master to support a serverless Puppet deployment
-#   Binds the Puppet master daemon to localhost for isolated catalog generation
+#   Configures a Puppetserver to support a "serverless" Puppet deployment
+#   Binds the Puppetserver daemon to localhost for isolated catalog generation
+#   Automatically generated CA environment includes "localhost" as a X509v3 SAN
 #
 # @example
 #   include profiles::bootstrap
 #
-class profiles::bootstrap::node::master() {
+# @param puppet_config
+#   Specify the absolute path to the global Puppet configuration file
+#
+# @param puppetserver_web_config
+#   Specify the absolute path to the Puppetserver Webserver configuration file
+#
+# @param r10k_binary
+#   Specify the absolute path to the r10k binary
+#
+# @param r10k_config
+#   Specify the absolute path to the r10k configuration file
+#   Tracked file within the penguinspiral/puppet-r10k Git repository
+#
+class profiles::bootstrap::node::server(
+  Stdlib::AbsolutePath $puppet_config           = $settings::config,
+  Stdlib::AbsolutePath $puppetserver_web_config = '/etc/puppetlabs/puppetserver/conf.d/webserver.conf',
+  Stdlib::AbsolutePath $r10k_binary             = '/usr/bin/r10k',
+  Stdlib::AbsolutePath $r10k_config             = '/etc/puppetlabs/r10k/r10k.yaml',
+) {
 
   file {
     default:
@@ -32,14 +51,14 @@ class profiles::bootstrap::node::master() {
   ini_setting {
     default:
       ensure            => present,
-      path              => '/etc/puppet/puppet.conf',
+      path              => $puppet_config,
       key_val_separator => '=',
     ;
 
-    'r10k_prerun':
+    'r10k_prerun_command':
       section => 'main',
       setting => 'prerun_command',
-      value   => '/usr/bin/r10k deploy environment --verbose --puppetfile --config /etc/puppet/r10k/r10k.yaml',
+      value   => "${r10k_binary} deploy environment --verbose --puppetfile --config ${r10k_config}",
     ;
 
     'puppet_master_dns_cert':
