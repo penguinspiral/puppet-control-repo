05ab2af5726b45945d4dae4a2521f76f084ee665
Myles Grindon <penguinspiral@users.noreply.github.com>
2022-05-17 01:37:10 -0700

    [puppet] Custom Facter fact "role" in profiles::bootstrap::agent
    Adhering to the "Roles & Profiles" pattern requires a node exposing its
    targeted "Role" in a way Puppet can consume and correctly assign the
    corresponding "Profiles".
    This repository utilises "Role" assignments via Hieradata lookups as
    per the original Hiera structure design, see: 50b984a.
    
    This "Role" information is surfaced as a custom Facter fact by means of
    an environment variable: `FACTER_ROLE`.
    Typically this environment variable is only exposed during the Debian
    GNU/Linux installer operational environment for *all* node "types".
    
    To ensure this "Role" assignment stays permanent the 'ghoneycutt-facter'
    module is leveraged to handle a static, hardcoded representation. The
    use of a class parameter facilitates the explicit overriding (e.g. via
    `fqdn/%{facts.fqdn}.yaml`) of "Role" if required.

--- site/profiles/manifests/bootstrap/agent.pp
+++ site/profiles/manifests/bootstrap/agent.pp
@@ -1,20 +1,30 @@
 # @summary
-#   Specifies the Puppet agent's targeted Puppetserver for obtain its catalog
+#   Populates the bootstrapped node's Puppet "role" as a local fact
+#   Specifies the Puppet agent's targeted Puppetserver for obtaining its catalog
 #   Configures the Puppet agent to run at startup via systemd
 #
 # @example
 #   include profiles::bootstrap
 #
+# @param role
+#   Specify the node's "role" (Roles & Profiles pattern)
+#   Specified as a kernel command-line parameter and sourced via env var: FACTER_ROLE
+#
 # @param puppet_config
-#   Specify the absolute path to the global Puppet configuration file
+#   Specify the absolute path of the global Puppet configuration file
 #
 # @param puppetserver
 #   Specify the hostname of the targeted Puppetserver
 #
-class profiles::bootstrap::node::agent(
+class profiles::bootstrap::agent(
+  String[1]            $role          = $facts['role'],
   Stdlib::AbsolutePath $puppet_config = $settings::config,
-  Stdlib::Host         $puppetserver = 'localhost',
+  Stdlib::Host         $puppetserver  = 'localhost',
 ) {
+
+  facter::fact { 'role':
+    value => $role,
+  }
 
   file { '/etc/systemd/system/multi-user.target.wants/puppet.service':
     ensure => link,
